fe671b538fd797d07f049e0b14885d2f81edd158
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-06 18:48:40 +0900

    `main` 실행시 광고 뜨도록 변경

--- python_poetry_template/main.py
+++ python_poetry_template/main.py
@@ -7,8 +7,7 @@ def show_ad():
 
 def main():
     print('연규컴퍼니')
-    # 왜 hello world 가 아니라 연규컴퍼니 인가요?
-    # https://github.com/code-yeongyu/comment-python-project/wiki/%EA%B8%B0%EB%B3%B8-%ED%85%9C%ED%94%8C%EB%A6%BF-%EC%8B%A4%ED%96%89-%EB%AC%B8%EA%B5%AC-%EA%B4%80%EB%A0%A8-%EB%85%BC%EC%9D%98-%EB%82%B4%EC%9A%A9
+    show_ad()
 
 
 if __name__ == '__main__':
