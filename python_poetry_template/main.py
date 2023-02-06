d71dc579a4a6b06b3afe180110c3407d7a5efdc4
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-06 18:48:18 +0900

    광고를 표시하는 함수 `show_ad` 추가

--- python_poetry_template/main.py
+++ python_poetry_template/main.py
@@ -1,3 +1,10 @@
+def show_ad():
+    """광고페이지를 띄워서 트래픽을 한번 받게 하고, 브라우저가 닫히면 프로그램을 종료한다."""
+    # request logic here
+    # some complex & hard to understand browser logic here
+    # some complex & hard to understand browser close detection here
+
+
 def main():
     print('연규컴퍼니')
     # 왜 hello world 가 아니라 연규컴퍼니 인가요?
