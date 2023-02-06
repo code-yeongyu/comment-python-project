e5e1e2c9ba89a47a7334ce91f18cc996cf09021d
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-02-06 17:57:52 +0900

    주석 추가

--- python_poetry_template/main.py
+++ python_poetry_template/main.py
@@ -1,6 +1,7 @@
 def main():
+    """프로그램의 main 함수로써 프로그램의 시작점이다."""
     print("Hello, world!")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': # 올바르게 스크립트가 실행되었을 경우에만 main 을 실행한다.
+    main() # main 호출
